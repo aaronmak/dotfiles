b301c388c4d82a29c39d2d7399fb5e0b5776fdae
Aaron Mak <im@arnmk.com>
2023-04-28 11:19:28 +0800

    Telescope should search hidden files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -252,8 +252,36 @@ function! RenameFile()
   endif
 endfunction
 ]])
+
 map('', '<Leader>rf', ':call RenameFile()<cr>')
 
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- don't search in the `.git` directory
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+		live_grep = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
 map('n', '<leader>ff', ':Telescope find_files<cr>')
 map('n', '<leader>fg', ':Telescope live_grep<cr>')
 map('n', '<leader>fb', ':Telescope buffers<cr>')
