03f9a6e83064d0cbf2fc74ca4150882f1694bd4e
Aaron Mak <im@arnmk.com>
2025-08-15 15:49:50 +0800

    Add config for indent animation

--- config/nvim/lua/plugins/display.lua
+++ config/nvim/lua/plugins/display.lua
@@ -68,6 +68,20 @@ return {
 					easing = "quad",
 				},
 			},
+			indent = {
+				-- your indent configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+				animate = {
+					enabled = vim.fn.has("nvim-0.10") == 1,
+					style = "out",
+					easing = "linear",
+					duration = {
+						step = 5, -- ms per step
+						total = 200, -- maximum duration
+					},
+				},
+			},
 		},
 	},
 }
